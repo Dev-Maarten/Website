d8e3e6544b5f872e154c98eb8401469ade3f5d2f
maarten <maartensitskoorn0@gmail.com>
2024-09-20 21:04:14 +0200

    flexboxhtmlcss-master

---
+++ main.js
@@ -0,0 +1,27 @@
+ piet(1,6);
+
+function piet(s,n) {
+    for (let x=s;x<n;x++) {
+        //console.log(x)
+        // if (x==1) {
+        //  console.log("bericht");
+        // } else {
+        //  console.log("anders");
+        // }
+
+        switch (x) {
+            case 1:
+                console.log("rood");
+                break;
+            case 2:
+                console.log("oranje");
+                break;
+            case 3:
+                console.log("groen");
+                break;
+            default:
+                console.log("anders");
+                break;
+        }
+    }
+}
